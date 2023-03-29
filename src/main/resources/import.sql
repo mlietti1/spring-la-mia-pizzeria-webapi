92cfd0ba056fc8eb228a0b27b859b1c74ba9585f
mlietti1 <mcf.lietti@outlook.com>
2023-03-29 17:55:57 +0200

    last commit

---
+++ src/main/resources/import.sql
@@ -0,0 +1,9 @@
+INSERT INTO db_pizzeria.pizzas
+(created_at, description, name, price, updated_at)
+VALUES('2023-03-29 17:33:50', 'Mozzarella, pomodoro, basilico', 'Margherita', 5.00, '2023-03-29 17:33:50');
+INSERT INTO db_pizzeria.pizzas
+(created_at, description, name, price, updated_at)
+VALUES('2023-03-29 17:34:50', 'Pomodoro, aglio', 'Marinara', 4.00, '2023-03-29 17:34:50');
+INSERT INTO db_pizzeria.pizzas 
+(created_at, description, name, price, updated_at)
+VALUES ('2023-03-29 17:34:50', 'Mozzarella, pomodoro, salame piccante', 'Diavola', 6.00, '2023-03-29 17:46:50');
