31f57f09f134a9bd7eff3e482bed29f818c04468
mlietti1 <mcf.lietti@outlook.com>
2023-03-30 14:46:43 +0200

    pizza svg e import sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -10,3 +10,13 @@ VALUES ('2023-03-29 17:34:50', 'Mozzarella, pomodoro, salame piccante', 'Diavola
 INSERT INTO db_pizzeria.pizzas 
 (created_at, description, name, price, updated_at)
 VALUES ('2023-03-30 11:33:50', 'Mozzarella, pomodoro, crudo, gorgonzola', 'Anna\'s favorite', 7.00, '2023-03-30 11:46:50');
+INSERT INTO db_pizzeria.pizzas 
+(created_at, description, name, price, updated_at)
+VALUES ('2023-03-30 13:33:50', 'Mozzarella, pomodoro, olive, cotto, carciofi, funghi', '4 Stagioni', 7.50, '2023-03-30 13:46:50');
+INSERT INTO db_pizzeria.pizzas 
+(created_at, description, name, price, updated_at)
+VALUES ('2023-03-30 11:33:50', 'Mozzarella, pomodoro, acciughe', 'Napoli', 6.00, '2023-03-30 11:46:50');
+INSERT INTO db_pizzeria.pizzas 
+(created_at, description, name, price, updated_at)
+VALUES ('2023-03-30 11:33:50', 'Mozzarella, grana, fontina, gorgonzola', '4 formaggi', 7.00, '2023-03-30 11:46:50');
+
