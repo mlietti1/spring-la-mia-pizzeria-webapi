416083c2e0fd5a9e249cd46017953b4f7c7703b2
mlietti1 <mcf.lietti@outlook.com>
2023-04-04 15:56:06 +0200

    fixed sql import

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,22 +1,7 @@
-INSERT INTO db_pizzeria.pizzas
-(created_at, description, name, price, updated_at)
-VALUES('2023-03-29 17:33:50', 'Mozzarella, pomodoro, basilico', 'Margherita', 5.00, '2023-03-29 17:33:50');
-INSERT INTO db_pizzeria.pizzas
-(created_at, description, name, price, updated_at)
-VALUES('2023-03-29 17:34:50', 'Pomodoro, aglio', 'Marinara', 4.00, '2023-03-29 17:34:50');
-INSERT INTO db_pizzeria.pizzas 
-(created_at, description, name, price, updated_at)
-VALUES ('2023-03-29 17:34:50', 'Mozzarella, pomodoro, salame piccante', 'Diavola', 6.00, '2023-03-29 17:46:50');
-INSERT INTO db_pizzeria.pizzas 
-(created_at, description, name, price, updated_at)
-VALUES ('2023-03-30 11:33:50', 'Mozzarella, pomodoro, crudo, gorgonzola', 'Anna\'s favorite', 7.00, '2023-03-30 11:46:50');
-INSERT INTO db_pizzeria.pizzas 
-(created_at, description, name, price, updated_at)
-VALUES ('2023-03-30 13:33:50', 'Mozzarella, pomodoro, olive, cotto, carciofi, funghi', '4 Stagioni', 7.50, '2023-03-30 13:46:50');
-INSERT INTO db_pizzeria.pizzas 
-(created_at, description, name, price, updated_at)
-VALUES ('2023-03-30 11:33:50', 'Mozzarella, pomodoro, acciughe', 'Napoli', 6.00, '2023-03-30 11:46:50');
-INSERT INTO db_pizzeria.pizzas 
-(created_at, description, name, price, updated_at)
-VALUES ('2023-03-30 11:33:50', 'Mozzarella, grana, fontina, gorgonzola', '4 formaggi', 7.00, '2023-03-30 11:46:50');
-
+INSERT INTO db_pizzeria.pizzas (created_at, description, name, price, updated_at) VALUES('2023-03-29 17:33:50', 'Mozzarella, pomodoro, basilico', 'Margherita', 5.00, '2023-03-29 17:33:50');
+INSERT INTO db_pizzeria.pizzas (created_at, description, name, price, updated_at) VALUES('2023-03-29 17:34:50', 'Pomodoro, aglio', 'Marinara', 4.00, '2023-03-29 17:34:50');
+INSERT INTO db_pizzeria.pizzas (created_at, description, name, price, updated_at) VALUES ('2023-03-29 17:34:50', 'Mozzarella, pomodoro, salame piccante', 'Diavola', 6.00, '2023-03-29 17:46:50');
+INSERT INTO db_pizzeria.pizzas (created_at, description, name, price, updated_at) VALUES ('2023-03-30 11:33:50', 'Mozzarella, pomodoro, crudo, gorgonzola', 'Anna\'s favorite', 7.00, '2023-03-30 11:46:50');
+INSERT INTO db_pizzeria.pizzas (created_at, description, name, price, updated_at) VALUES ('2023-03-30 13:33:50', 'Mozzarella, pomodoro, olive, cotto, carciofi, funghi', '4 Stagioni', 7.50, '2023-03-30 13:46:50');
+INSERT INTO db_pizzeria.pizzas (created_at, description, name, price, updated_at) VALUES ('2023-03-30 11:33:50', 'Mozzarella, pomodoro, acciughe', 'Napoli', 6.00, '2023-03-30 11:46:50');
+INSERT INTO db_pizzeria.pizzas (created_at, description, name, price, updated_at) VALUES ('2023-03-30 11:33:50', 'Mozzarella, grana, fontina, gorgonzola', '4 formaggi', 7.00, '2023-03-30 11:46:50');
