b0eabe173fd14dd792378ecd3a345985fdcc151d
mlietti1 <mcf.lietti@outlook.com>
2023-04-06 11:37:11 +0200

    edit offer

--- src/main/java/org/lessons/java/springlamiapizzeriacrud/controller/OfferController.java
+++ src/main/java/org/lessons/java/springlamiapizzeriacrud/controller/OfferController.java
@@ -1,6 +1,7 @@
 package org.lessons.java.springlamiapizzeriacrud.controller;
 
 import jakarta.validation.Valid;
+import org.lessons.java.springlamiapizzeriacrud.exceptions.OfferNotFoundException;
 import org.lessons.java.springlamiapizzeriacrud.exceptions.PizzaNotFoundException;
 import org.lessons.java.springlamiapizzeriacrud.model.Offer;
 import org.lessons.java.springlamiapizzeriacrud.model.Pizza;
@@ -41,15 +42,36 @@ public class OfferController {
         }
         model.addAttribute("offer", offer);
 
-        return "/offers/create";
+        return "/offers/edit";
     }
 
     @PostMapping("/create")
     public String doCreate(@Valid @ModelAttribute Offer formOffer, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
-            return "/offers/create";
+            return "/offers/edit";
         }
         Offer createdOffer = offerService.create(formOffer);
         return "redirect:/pizzas/" + Integer.toString(createdOffer.getPizza().getId());
+    }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable Integer id, Model model) {
+
+        try {
+            Offer offer = offerService.getById(id);
+            model.addAttribute("offer", offer);
+            return "offers/edit";
+        } catch (OfferNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Offer with id " + id + " not found.");
+        }
+    }
+
+    @PostMapping("edit/{id}")
+    public String doEdit(@PathVariable Integer id, @Valid @ModelAttribute("offer") Offer formOffer, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "offers/edit";
+        }
+        Offer updatedOffer = offerService.update(formOffer);
+        return "redirect:/pizzas/" + Integer.toString(updatedOffer.getPizza().getId());
     }
 }
--- src/main/java/org/lessons/java/springlamiapizzeriacrud/service/OfferService.java
+++ src/main/java/org/lessons/java/springlamiapizzeriacrud/service/OfferService.java
@@ -1,11 +1,13 @@
 package org.lessons.java.springlamiapizzeriacrud.service;
 
+import org.lessons.java.springlamiapizzeriacrud.exceptions.OfferNotFoundException;
 import org.lessons.java.springlamiapizzeriacrud.model.Offer;
 import org.lessons.java.springlamiapizzeriacrud.repository.OfferRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 @Service
 public class OfferService {
@@ -15,5 +17,19 @@ public class OfferService {
     public Offer create(Offer formOffer) {
 
         return offerRepository.save(formOffer);
+    }
+
+    public Offer update(Offer formOffer) throws OfferNotFoundException {
+
+        return offerRepository.save(formOffer);
+    }
+
+    public Offer getById(Integer id) throws OfferNotFoundException {
+        Optional<Offer> result = offerRepository.findById(id);
+        if (result.isPresent()) {
+            return result.get();
+        } else {
+            throw new OfferNotFoundException(Integer.toString(id));
+        }
     }
 }
