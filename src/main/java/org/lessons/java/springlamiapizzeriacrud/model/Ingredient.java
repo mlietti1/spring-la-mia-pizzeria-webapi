7e503cd1392f7699212a9e925828e2489ad5c628
mlietti1 <mcf.lietti@outlook.com>
2023-04-07 17:20:20 +0200

    primo commit

--- src/main/java/org/lessons/java/springlamiapizzeriacrud/model/Ingredient.java
+++ src/main/java/org/lessons/java/springlamiapizzeriacrud/model/Ingredient.java
@@ -35,4 +35,17 @@ public class Ingredient {
     public void setName(String name) {
         this.name = name;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ingredient that = (Ingredient) o;
+        return Objects.equals(id, that.id) && Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
 }
